9fea53277c90fd9dcf4cea1586b54c009565da0a
Cazaks <calebezak@gmail.com>
2025-12-17 19:17:41 +0100

    main method to test my composition

--- src/composition/EmployeeTest.java
+++ src/composition/EmployeeTest.java
@@ -1,4 +1,13 @@
 package composition;
 
 public class EmployeeTest {
+    public static void main(String[] args) {
+
+        Date birthdate = new Date(11, 6, 1989);
+        Date hiredate = new Date(1, 10, 2015);
+
+        Employee hired = new Employee("James", "Brown", hiredate, birthdate);
+
+        System.out.println(hired);
+    }
 }
