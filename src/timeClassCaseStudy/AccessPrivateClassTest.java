434fb40758f0a51577b085e6cacb7a69bfa5f65d
Cazaks <calebezak@gmail.com>
2025-12-17 12:21:29 +0100

    Trying to access private members of Time1 class example

---
+++ src/timeClassCaseStudy/AccessPrivateClassTest.java
@@ -0,0 +1,15 @@
+package timeClassCaseStudy;
+
+public class AccessPrivateClassTest {
+
+    public static void main(String[] args) {
+
+        Time1 time = new Time1();
+
+//        time.hour = 7;
+//        time.minute = 5;
+//        time.second = 55;
+
+//        This will not work because that fields are private method in Time1 class
+    }
+}
