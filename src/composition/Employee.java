82eeebbc2f1a561b6af3d727006550f45b8b37db
Cazaks <calebezak@gmail.com>
2025-12-17 19:03:00 +0100

    Employee class for composition.......

--- src/composition/Employee.java
+++ src/composition/Employee.java
@@ -1,4 +1,20 @@
 package composition;
 
 public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+    private Date hireDate;
+
+    public Employee(String firstName, String lastName, Date dateOfBirth, Date hireDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.hireDate = hireDate;
+    }
+
+    public String toString() {
+        return String.format("%s %s Hired: %s Birthday: %s", firstName, lastName, hireDate, dateOfBirth);
+    }
 }
